9d38313628f1043c92a9c8a8ad5877bba488ee58
Anthony Miyaguchi <acmiyaguchi@gatech.edu>
2023-01-02 03:29:30 +0000

    Move concat script into luigi script

--- workflows/birdnet.py
+++ workflows/birdnet.py
@@ -1,8 +1,13 @@
-import luigi
-from luigi.contrib.docker_runner import DockerTask
-from pathlib import Path
 import os
 import tempfile
+from functools import partial
+from multiprocessing import Pool
+from pathlib import Path
+
+import luigi
+import pandas as pd
+from luigi.contrib.docker_runner import DockerTask
+from tqdm import tqdm
 
 
 class BirdNetAnalyzeTask(DockerTask):
@@ -52,10 +57,10 @@ class BirdNetAnalyzeTask(DockerTask):
 
 
 # wrapper task to run BirdNet on all species
-class BirdNetClassifyAllTask(luigi.WrapperTask):
+class BirdNetAnalyzeTaskAllTask(luigi.WrapperTask):
     birdclef_root_path = luigi.Parameter()
     output_path = luigi.Parameter()
-    n_threads = luigi.IntParameter(default=os.cpu_count())
+    n_threads = luigi.IntParameter(default=4)
 
     def requires(self):
         species = [
@@ -70,15 +75,106 @@ class BirdNetClassifyAllTask(luigi.WrapperTask):
             )
 
 
+class BirdNetAnalyzeConcatTask(luigi.Task):
+    birdclef_root_path = luigi.Parameter()
+    taxonomy_path = luigi.Parameter()
+    input_path = luigi.Parameter()
+    output_path = luigi.Parameter()
+    parallelism = luigi.IntParameter(default=os.cpu_count())
+
+    def requires(self):
+        return [
+            BirdNetAnalyzeTaskAllTask(
+                birdclef_root_path=self.birdclef_root_path,
+                output_path=self.input_path,
+            )
+        ]
+
+    def output(self):
+        return luigi.LocalTarget(f"{self.output_path}/birdnet-analysis.parquet")
+
+    def read_path(self, path: Path, lookup=lambda x: x) -> pd.DataFrame:
+        df = pd.read_csv(path)
+        name = path.name.split(".")[0]
+        df["filename"] = f"{path.parent.name}/{name}.ogg"
+        df["birdnet_label"] = df["Common name"].apply(lambda x: lookup(x, "unknown"))
+        df["birdnet_common_name"] = df["Common name"]
+        return df
+
+    def run(self):
+        taxonomy_df = pd.read_csv(self.taxonomy_path)
+        common_to_code = dict(
+            list(zip(taxonomy_df["PRIMARY_COM_NAME"], taxonomy_df["SPECIES_CODE"]))
+        )
+
+        root = Path(self.input_path)
+        paths = sorted(root.glob("**/*.csv"))
+        with Pool(self.parallelism) as pool:
+            res = list(
+                tqdm(
+                    pool.imap(
+                        partial(self.read_path, lookup=common_to_code.get), paths
+                    ),
+                    total=len(paths),
+                )
+            )
+        df = pd.concat(res).rename(
+            columns={
+                "Start (s)": "start_sec",
+                "End (s)": "end_sec",
+                "Confidence": "confidence",
+            }
+        )[
+            [
+                "start_sec",
+                "end_sec",
+                "confidence",
+                "birdnet_label",
+                "birdnet_common_name",
+                "filename",
+            ]
+        ]
+        print(df.head())
+        # ensure the output directory exists
+        output_path = Path(self.output().path)
+        output_path.parent.mkdir(parents=True, exist_ok=True)
+        df.to_parquet(output_path)
+
+
+class BirdNetTask(luigi.WrapperTask):
+    """Wrapper around the entire DAG."""
+
+    birdclef_root_path = luigi.Parameter()
+    intermediate_path = luigi.Parameter()
+    output_path = luigi.Parameter()
+    n_threads = luigi.IntParameter(default=4)
+    parallelism = luigi.IntParameter(default=os.cpu_count())
+
+    def requires(self):
+        yield BirdNetAnalyzeTaskAllTask(
+            birdclef_root_path=self.birdclef_root_path,
+            output_path=f"{self.intermediate_path}/birdnet/analysis",
+        )
+        yield BirdNetAnalyzeConcatTask(
+            birdclef_root_path=self.birdclef_root_path,
+            taxonomy_path=f"{self.birdclef_root_path}/eBird_Taxonomy_v2021.csv",
+            input_path=f"{self.intermediate_path}/birdnet/analysis",
+            output_path=f"{self.output_path}/birdnet",
+            parallelism=self.parallelism,
+        )
+
+
 if __name__ == "__main__":
+    n_threads = 4
     luigi.build(
         [
-            BirdNetClassifyAllTask(
+            BirdNetTask(
                 birdclef_root_path="data/raw/birdclef-2022",
-                output_path="data/intermediate/birdclef-2022/birdnet/analyze",
-                n_threads=4,
+                intermediate_path="data/intermediate/birdclef-2022",
+                output_path="data/processed/birdclef-2022",
+                n_threads=n_threads,
+                parallelism=os.cpu_count(),
             )
         ],
-        log_level="INFO",
-        workers=os.cpu_count() // 4,
+        workers=os.cpu_count() // n_threads,
     )
