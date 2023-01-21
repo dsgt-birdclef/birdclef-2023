e749919460d32ec17efa57dedf792f0b52acaeda
Anthony Miyaguchi <acmiyaguchi@gatech.edu>
2023-01-21 06:00:50 +0000

    Add docker-compose and docker file for building site

--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>DS@GT BirdCLEF 2023</h1>
+
+<p>This page contains work for the BirdCLEF 2023 competition.</p>
