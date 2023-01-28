f0d57058cb0ecdd279a756128b4eafcb9c7c1638
Anthony Miyaguchi <acmiyaguchi@gatech.edu>
2023-01-27 23:49:27 -0800

    fix: import DateTime from luxon instead of default

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   import "sakura.css/css/sakura.css";
-  import luxon from "luxon";
+  import { DateTime } from "luxon";
 
   export let data: any;
   let github_url = "https://github.com/dsgt-birdclef/birdclef-2023";
   $: client_status = data && data.client_status;
-  $: build_time = client_status && luxon.DateTime.fromISO(client_status.build_time);
+  $: build_time = client_status && DateTime.fromISO(client_status.build_time);
 </script>
 
 <main class="container">
@@ -18,7 +18,7 @@
       <div>
         <b>app</b>:
         <a href="{github_url}/commit/{client_status.commit_sha}">v{client_status.version}</a>
-        <b>build time</b>: {build_time.toLocaleString(luxon.DateTime.DATETIME_MED)}
+        <b>build time</b>: {build_time.toLocaleString(DateTime.DATETIME_MED)}
       </div>
     {/if}
   </nav>
