72250ef001286ed141386c149befa9731c1f5f89
GQW19 <>
2023-03-07 03:42:41 +0000

    Working Mixit Model DockerFiles and runner scripts.

--- scripts/mixit_batch.sh
+++ scripts/mixit_batch.sh
@@ -5,6 +5,8 @@ n_threads=${N_THREADS:-8}
 
 species=${1:?Please provide a species name.}
 
+gsutil -m cp -r gs://birdclef-2023/data/raw/birdclef-2022/train_audio/${species} ~/birdclef-2023/data/raw/birdclef-2022/train_audio/
+
 cd "$(dirname "$0")/.."
 raw_prefix=data/raw/birdclef-2022/train_audio
 processed_prefix=data/processed/mixit/analysis
@@ -19,10 +21,10 @@ fi
 for path in ${raw_prefix}/${species}/*.ogg; do
     echo "$(date --iso-8601=seconds) Processing ${path}" | tee -a ${logs_file}
     audio_file=$(basename $path)
+    # Currently does not run GPU version         --gpus=all \
     docker run --rm \
-        --gpus=all \
         -v ${PWD}/data:/mnt/data \
-        -it us-central1-docker.pkg.dev/birdclef-eda-f22/birdclef-eda-f22/bird-mixit-gpu:latest \
+        -it us-central1-docker.pkg.dev/birdclef-2023/birdclef-2023/bird-mixit-gpu:latest \
         python scripts/mixit_ogg_wrapper.py \
         --input /mnt/${path} \
         --output /mnt/${processed_prefix}/${species}/${audio_file} \
@@ -31,11 +33,11 @@ for path in ${raw_prefix}/${species}/*.ogg; do
 done
 
 # now run birdnet on the output of mixit
-docker run --rm \
-    -v ${PWD}/data:/mnt/data \
-    -it us-central1-docker.pkg.dev/birdclef-eda-f22/birdclef-eda-f22/birdnet:latest \
-    analyze.py \
-    --i /mnt/${processed_prefix}/${species} \
-    --o /mnt/${processed_prefix}/${species} \
-    --threads ${n_threads} \
-    --rtype csv | tee -a ${logs_file}
+#docker run --rm \
+#    -v ${PWD}/data:/mnt/data \
+#    -it us-central1-docker.pkg.dev/birdclef-2023/birdclef-2023/birdnet:latest \
+#    analyze.py \
+#    --i /mnt/${processed_prefix}/${species} \
+#    --o /mnt/${processed_prefix}/${species} \
+#    --threads ${n_threads} \
+#    --rtype csv | tee -a ${logs_file}
