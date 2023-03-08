d5a4d894f3f3bb3b2844a021e28e5d99f3af6de5
Anthony Miyaguchi <acmiyaguchi@gatech.edu>
2023-03-07 20:41:34 -0800

    Add TrainDurationsWorkflow task

--- workflows/train_durations.py
+++ workflows/train_durations.py
@@ -1,10 +1,13 @@
 import os
+import tempfile
 from multiprocessing import Pool
 from pathlib import Path
 
 import librosa
 import luigi
 import pandas as pd
+from luigi.contrib.external_program import ExternalProgramTask
+from luigi.parameter import ParameterVisibility
 from tqdm import tqdm
 
 
@@ -14,16 +17,23 @@ def read_path(path: Path) -> dict:
     return dict(filename="/".join(path.parts[-2:]), duration=duration)
 
 
-class TrainDurations(luigi.Task):
+class DynamicRequiresMixin:
+    dynamic_requires = luigi.Parameter([], visibility=ParameterVisibility.HIDDEN)
+
+    def requires(self):
+        return self.dynamic_requires
+
+
+class TrainDurations(luigi.Task, DynamicRequiresMixin):
     """Get the duration of each song in the training set into a parquet file."""
 
     birdclef_root_path = luigi.Parameter()
     output_path = luigi.Parameter()
-    parallelism = luigi.IntParameter(default=os.cpu_count() // 2)
+    parallelism = luigi.IntParameter(default=os.cpu_count())
     capture_output = True
 
     def requires(self):
-        return []
+        return self.dynamic_requires
 
     def output(self):
         return luigi.LocalTarget(self.output_path)
@@ -38,6 +48,47 @@ class TrainDurations(luigi.Task):
         # ensure the output directory exists
         Path(self.output_path).parent.mkdir(parents=True, exist_ok=True)
         df.to_parquet(self.output_path)
+
+
+class GSUtilRsyncTask(ExternalProgramTask, DynamicRequiresMixin):
+    input_path = luigi.Parameter()
+    output_path = luigi.Parameter()
+    capture_output = True
+
+    def program_args(self):
+        return f"gsutil -m -r rsync {self.input_path} {self.output_path}"
+
+
+class TrainDurationsWorkflow(luigi.WrapperTask):
+    birdclef_root_path = luigi.Parameter()
+    output_path = luigi.Parameter()
+    parallelism = luigi.IntParameter(default=os.cpu_count())
+
+    def requires(self):
+        # create a temporary path to download the data
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            tmp_path = Path(tmp_dir)
+            tmp_birdclef_root_path = tmp_path / "birdclef-2022"
+            tmp_output_path = tmp_path / "train_durations.parquet"
+
+            download_task = GSUtilRsyncTask(
+                input_path=self.birdclef_root_path,
+                output_path=tmp_birdclef_root_path,
+            )
+            yield download_task
+
+            train_durations = TrainDurations(
+                birdclef_root_path=tmp_birdclef_root_path,
+                output_path=tmp_output_path,
+                parallelism=self.parallelism,
+            )
+            yield train_durations
+
+            upload_task = GSUtilRsyncTask(
+                input_path=tmp_output_path,
+                output_path=self.output_path,
+            )
+            yield upload_task
 
 
 if __name__ == "__main__":
