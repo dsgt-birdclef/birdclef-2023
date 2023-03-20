ead34674e49ef9c4fa35fe6e447d241ddd98d897
GQW19 <99105662+GQW19@users.noreply.github.com>
2023-03-20 01:37:55 -0400

    feat: Working Mixit Model DockerFiles and runner scripts. (#59)
    * Working Mixit Model DockerFiles and runner scripts.
    
    * Update build for submodule
    
    * Update mixit batch script for optional gpu support
    
    ---------
    
    Co-authored-by: GQW19 <>
    Co-authored-by: Anthony Miyaguchi <acmiyaguchi@gatech.edu>

--- scripts/mixit_batch.sh
+++ scripts/mixit_batch.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+gpu_enabled=${GPU_ENABLED:-0}
 n_threads=${N_THREADS:-8}
 
 species=${1:?Please provide a species name.}
+birdclef_name=${2:-birdclef-2023}
+
 
 cd "$(dirname "$0")/.."
-raw_prefix=data/raw/birdclef-2022/train_audio
-processed_prefix=data/processed/mixit/analysis
-logs_file=data/processed/mixit/analysis/batch_analysis_${species}.log
+raw_prefix=data/raw/${birdclef_name}/train_audio
+processed_prefix=data/processed/${birdclef_name}/mixit/analysis
+logs_file=data/processed/${birdclef_name}/mixit/analysis/batch_analysis_${species}.log
+
+gsutil -m rsync -r \
+    gs://birdclef-2023/${raw_prefix}/${species}/ \
+    ${raw_prefix}/${species}/
 
 mkdir -p $(dirname ${logs_file})
 
@@ -20,22 +27,12 @@ for path in ${raw_prefix}/${species}/*.ogg; do
     echo "$(date --iso-8601=seconds) Processing ${path}" | tee -a ${logs_file}
     audio_file=$(basename $path)
     docker run --rm \
-        --gpus=all \
+        $(if [[ ! $gpu_enabled -eq 0 ]]; then echo "--gpus=all"; fi) \
         -v ${PWD}/data:/mnt/data \
-        -it us-central1-docker.pkg.dev/birdclef-eda-f22/birdclef-eda-f22/bird-mixit-gpu:latest \
+        -it us-central1-docker.pkg.dev/birdclef-2023/birdclef-2023/bird-mixit-gpu:latest \
         python scripts/mixit_ogg_wrapper.py \
         --input /mnt/${path} \
         --output /mnt/${processed_prefix}/${species}/${audio_file} \
         --model_name output_sources4 \
         --num_sources 4 | tee -a ${logs_file}
 done
-
-# now run birdnet on the output of mixit
-docker run --rm \
-    -v ${PWD}/data:/mnt/data \
-    -it us-central1-docker.pkg.dev/birdclef-eda-f22/birdclef-eda-f22/birdnet:latest \
-    analyze.py \
-    --i /mnt/${processed_prefix}/${species} \
-    --o /mnt/${processed_prefix}/${species} \
-    --threads ${n_threads} \
-    --rtype csv | tee -a ${logs_file}
