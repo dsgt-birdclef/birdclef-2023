adc85915eb7d292b9b9c5f0c89fdc4394ca65f77
Anthony Miyaguchi <acmiyaguchi@gatech.edu>
2023-03-24 05:32:11 +0000

    feat: add completed workflow for mixit assets

--- birdclef/birdnet.py
+++ birdclef/birdnet.py
@@ -3,8 +3,11 @@ This module contains some utility functions for dealing with the birdnet model.
 
 We write our own wrappers over the raw keras model.
 """
+import json
+from pathlib import Path
 from typing import Optional
 
+import numpy as np
 from tensorflow import keras
 
 
@@ -23,6 +26,12 @@ def load_model(path: str, model_attr: Optional[str] = "model") -> keras.Model:
     if model_attr:
         model = getattr(model, model_attr)
     return model
+
+
+def load_model_from_repo(birdnet_root: str) -> keras.Model:
+    return load_model(
+        Path(birdnet_root) / "checkpoints/V2.2/BirdNET_GLOBAL_3K_V2.2_Model/"
+    )
 
 
 def embedding_func(model: keras.Model):
@@ -49,3 +58,27 @@ def prediction_func(model: keras.Model):
         The function that can be used to extract the prediction layer.
     """
     return keras.backend.function([model.input], [model.layers[-1].output])
+
+
+def load_labels(birdnet_root: str):
+    path = Path(birdnet_root) / "checkpoints/V2.2/BirdNET_GLOBAL_3K_V2.2_Labels.txt"
+    labels = Path(path).read_text().splitlines()
+    return labels
+
+
+def load_mapped_labels(birdnet_root: str):
+    path = Path(birdnet_root) / "eBird_taxonomy_codes_2021E.json"
+    labels = load_labels(birdnet_root)
+    mapping = json.loads(path.read_text())
+    mapped_labels = [mapping[label] for label in labels]
+    return mapped_labels
+
+
+def rank_indices(pred, k=10):
+    """
+    Rank the indices of the predictions.
+    """
+    topk = np.argsort(pred, axis=1)[:, -k * 2 :]
+    values, counts = np.unique(topk, return_counts=True)
+    ind = np.argpartition(-counts, kth=k)[:k]
+    return values[ind]
--- workflows/mixit/visualize_subset.py
+++ workflows/mixit/visualize_subset.py
@@ -1,12 +1,20 @@
+import json
 import os
 from pathlib import Path
 
 import librosa
 import luigi
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
+from birdclef import birdnet
+from birdclef.data.utils import slice_seconds
 from birdclef.utils import spark_resource
+from workflows.convert_audio.ogg_to_mp3 import OggToMP3SingleTask
 from workflows.utils.gcs import single_file_target
+from workflows.utils.mixin import DynamicRequiresMixin
+from workflows.utils.rsync import GSUtilRsyncTask
 
 from .docker import MixitDockerTask
 
@@ -33,21 +41,123 @@ class GetTrackSubset(luigi.Task):
             subset.toPandas().to_json(self.output().path, orient="records")
 
 
+class GenerateAssets(luigi.Task, DynamicRequiresMixin):
+    output_path = luigi.Parameter()
+    track_name = luigi.Parameter()
+    birdnet_root = luigi.Parameter()
+
+    def output(self):
+        return single_file_target(
+            Path(self.output_path) / "assets" / Path(self.track_name).stem / "_SUCCESS"
+        )
+
+    def run(self):
+        # generate spectrograms
+        parent = Path(self.output().path).parent
+        parent.mkdir(parents=True, exist_ok=True)
+
+        paths = sorted(
+            Path(self.output_path).glob(f"audio/**/{Path(self.track_name).stem}*.mp3")
+        )
+        sr = 48_000
+        series = [librosa.load(path, sr=sr)[0] for path in paths]
+        # spectogram plots
+        for y, path in zip(series, paths):
+            plt.figure(figsize=(10, 3))
+            S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128)
+            S_dB = librosa.power_to_db(S, ref=np.max)
+            librosa.display.specshow(S_dB, sr=sr, x_axis="time", y_axis="mel")
+            plt.colorbar(format="%+2.0f dB")
+            plt.title(f"Mel-frequency spectrogram of {path.parts[-2]}/{path.parts[-1]}")
+            plt.tight_layout()
+            plt.savefig(f"{parent}/{path.stem}_spectogram.png")
+            plt.close()
+
+        # now lets generate the plots using the labels
+        model = birdnet.load_model_from_repo(self.birdnet_root)
+        prediction_func = birdnet.prediction_func(model)
+        labels = birdnet.load_labels(self.birdnet_root)
+        mapped_labels = birdnet.load_mapped_labels(self.birdnet_root)
+
+        # plots for top and bottom 5
+        for y, path in zip(series, paths):
+            X = slice_seconds(y, sr, seconds=3, step=1)
+            pred = prediction_func(X)[0]
+            # create a subplot
+            fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+            for idx in birdnet.rank_indices(pred, k=5):
+                ax[0].plot(pred[:, idx], label=labels[idx])
+                ax[0].set_title("top 5 predictions")
+                ax[0].legend()
+            for idx in birdnet.rank_indices(-pred, k=5):
+                ax[1].plot(pred[:, idx], label=labels[idx])
+                ax[1].set_title("bottom 5 predictions")
+                ax[1].legend()
+            plt.savefig(f"{parent}/{path.stem}_top5.png")
+
+            # write out the top 10 predictions and bottom 10 predictions
+            top10 = birdnet.rank_indices(pred, k=10)
+            bottom10 = birdnet.rank_indices(-pred, k=10)
+
+            # convert these into json
+            res = []
+            for idx in top10:
+                res.append(
+                    {
+                        "label": labels[idx],
+                        "mapped_label": mapped_labels[idx],
+                        "y": pred[:, idx].tolist(),
+                    }
+                )
+            Path(f"{parent}/{path.stem}_top10.json").write_text(json.dumps(res))
+
+            res = []
+            for idx in bottom10:
+                res.append(
+                    {
+                        "label": labels[idx],
+                        "mapped_label": mapped_labels[idx],
+                        "y": pred[:, idx].tolist(),
+                    }
+                )
+            Path(f"{parent}/{path.stem}_bottom10.json").write_text(json.dumps(res))
+
+        # touch the success file
+        Path(self.output().path).touch()
+
+
 class MixitWrapperTask(luigi.WrapperTask):
     birdclef_root_path = luigi.Parameter()
     track_subset_path = luigi.Parameter()
     output_path = luigi.Parameter()
+    birdnet_root_path = luigi.Parameter()
 
     def requires(self):
         track_subset_df = pd.read_json(self.track_subset_path)
         for row in track_subset_df.itertuples():
-            task = MixitDockerTask(
+            mixit_task = MixitDockerTask(
                 birdclef_root_path=self.birdclef_root_path,
                 output_path=f"{self.output_path}/audio",
                 track_name=row.filename,
                 num_sources=4,
             )
-            yield task
+            yield mixit_task
+
+            # let's also generate the original audio
+            mp3_task = OggToMP3SingleTask(
+                input_path=f"{self.birdclef_root_path}/train_audio",
+                output_path=f"{self.output_path}/audio",
+                track_name=row.filename,
+            )
+            yield mp3_task
+
+            generate_task = GenerateAssets(
+                output_path=self.output_path,
+                track_name=row.filename,
+                birdnet_root=self.birdnet_root_path,
+                dynamic_requires=[mixit_task, mp3_task],
+            )
+            yield generate_task
 
 
 # lets create a task that runs mixit on our subset of data
@@ -55,6 +165,7 @@ class VisualizeSubsetWorkflow(luigi.Task):
     birdclef_root_path = luigi.Parameter()
     durations_path = luigi.Parameter()
     output_path = luigi.Parameter()
+    birdnet_root_path = luigi.Parameter()
     limit = luigi.IntParameter(default=16)
 
     def run(self):
@@ -68,6 +179,7 @@ class VisualizeSubsetWorkflow(luigi.Task):
         yield MixitWrapperTask(
             birdclef_root_path=self.birdclef_root_path,
             track_subset_path=track_subset.output().path,
+            birdnet_root_path=self.birdnet_root_path,
             output_path=self.output_path,
         )
 
@@ -79,6 +191,7 @@ if __name__ == "__main__":
                 birdclef_root_path="data/raw/birdclef-2023",
                 durations_path="gs://birdclef-2023/data/processed/birdclef-2023/train_durations_v2.parquet",
                 output_path="data/processed/mixit_visualize_subset",
+                birdnet_root_path="vendor/BirdNET-Analyzer",
             )
         ],
         workers=max(os.cpu_count() // 2, 1),
