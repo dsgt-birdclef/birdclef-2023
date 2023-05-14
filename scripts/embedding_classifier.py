759abfe8dd194cb57f89a85003e2a1002839a067
Nathan <nzhongathan@gmail.com>
2023-05-14 03:02:45 +0000

    param search for log regression

--- scripts/embedding_classifier.py
+++ scripts/embedding_classifier.py
@@ -1,20 +1,78 @@
 import argparse
 import csv
 import pickle
+from pathlib import Path
+import tqdm
+import librosa
 
 import pyspark.sql.functions as F
 from pyspark.sql.functions import concat, rand
 
+from birdclef import birdnet
 from birdclef.utils import get_spark
+from birdclef.data.utils import slice_seconds
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", choices=["logistic_reg_3"], required=True, type=str)
+    parser.add_argument("--birdnet_path", default="data/models/birdnet-analyzer-pruned", type=str)
     parser.add_argument("--input", "--i", required=True, type=str)
+    parser.add_argument("--birdclef_root", default="data/raw/birdclef-2023", type=str)
+    parser.add_argument("--model", choices=["logistic_reg_3"], required=True, type=str)
+    
     parser.add_argument("--output", "--o", required=True, type=str)
     args, other_args = parser.parse_known_args()
     args = vars(args)
+
+    train_audio_root = Path(args["birdclef_root"]) / "train_audio"
+    species = sorted([p.name for p in train_audio_root.glob("*")])
+
+    for s in species:
+        species_root = train_audio_root / s
+        track_names = sorted(
+            ["/".join(p.parts[-2:]) for p in species_root.glob("*.ogg")]
+        )
+
+        for track_name in track_names:
+            # pad audio noise
+            
+
+    repo_path = args["birdnet_path"]
+    model = birdnet.load_model_from_repo(repo_path)
+    prediction_func = birdnet.prediction_func(model)
+    labels = birdnet.load_labels(repo_path)
+    mapped_labels = birdnet.load_mapped_labels(repo_path)
+
+    
+
+    rows = []
+    paths = sorted(Path(args["input"]).glob(f"{self.track_stem}*"))
+    for path in tqdm.tqdm(paths):
+        y, sr = librosa.load(path.as_posix(), sr=48_000)
+
+        X = slice_seconds(y, sr, seconds=3, step=3)
+        pred = prediction_func(X)[0]
+        pred_sigmoid = 1 / (1 + np.exp(-pred))
+
+        indices = birdnet.rank_indices(pred_sigmoid)
+        for i in range(pred_sigmoid.shape[0]):
+            predictions = [
+                {
+                    "index": int(j),
+                    "label": labels[j],
+                    "mapped_label": mapped_labels[j],
+                    "probability": float(pred_sigmoid[i][j]),
+                    "rank": rank,
+                }
+                for rank, j in enumerate(indices)
+            ]
+            rows.append(
+                {
+                    "track_name": "/".join(path.parts[-2:]),
+                    "embedding": pred[i].tolist(),
+                    "predictions": predictions,
+                }
+            )
 
     spark = get_spark()
     df = spark.read.parquet(args["input"])
