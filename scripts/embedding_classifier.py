101aec3b70eeca4f89aab6766668a491945659a6
Anthony Miyaguchi <acmiyaguchi@gatech.edu>
2023-05-15 21:24:58 -0400

    Add notebooks for more baseline classifiers (#94)
    * baseline dataset and work on classifier
    
    * cleaned up outputs
    
    * updated classifiers notebook
    
    * script and downloaded models
    
    * submission.csv in notebook
    
    * cleaned up nb and editted csv creation
    
    * nb didn't save in last commit
    
    * precommit
    
    * param search for log regression
    
    * Add notebook with svc
    
    * negative samples
    
    * Add best estimator
    
    * Fix linting issues
    
    ---------
    
    Co-authored-by: Nathan <nzhongathan@gmail.com>

--- scripts/embedding_classifier.py
+++ scripts/embedding_classifier.py
@@ -1,20 +1,64 @@
 import argparse
 import csv
 import pickle
+from pathlib import Path
 
+import librosa
 import pyspark.sql.functions as F
+import tqdm
 from pyspark.sql.functions import concat, rand
 
+from birdclef import birdnet
+from birdclef.data.utils import slice_seconds
 from birdclef.utils import get_spark
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", choices=["logistic_reg_3"], required=True, type=str)
+    parser.add_argument(
+        "--birdnet_path", default="data/models/birdnet-analyzer-pruned", type=str
+    )
     parser.add_argument("--input", "--i", required=True, type=str)
+    parser.add_argument("--birdclef_root", default="data/raw/birdclef-2023", type=str)
+    parser.add_argument("--model", choices=["logistic_reg_3"], required=True, type=str)
     parser.add_argument("--output", "--o", required=True, type=str)
     args, other_args = parser.parse_known_args()
     args = vars(args)
+
+    repo_path = args["birdnet_path"]
+    model = birdnet.load_model_from_repo(repo_path)
+    prediction_func = birdnet.prediction_func(model)
+    labels = birdnet.load_labels(repo_path)
+    mapped_labels = birdnet.load_mapped_labels(repo_path)
+
+    rows = []
+    paths = sorted(Path(args["input"]).glob(f"{self.track_stem}*"))
+    for path in tqdm.tqdm(paths):
+        y, sr = librosa.load(path.as_posix(), sr=48_000)
+
+        X = slice_seconds(y, sr, seconds=3, step=3)
+        pred = prediction_func(X)[0]
+        pred_sigmoid = 1 / (1 + np.exp(-pred))
+
+        indices = birdnet.rank_indices(pred_sigmoid)
+        for i in range(pred_sigmoid.shape[0]):
+            predictions = [
+                {
+                    "index": int(j),
+                    "label": labels[j],
+                    "mapped_label": mapped_labels[j],
+                    "probability": float(pred_sigmoid[i][j]),
+                    "rank": rank,
+                }
+                for rank, j in enumerate(indices)
+            ]
+            rows.append(
+                {
+                    "track_name": "/".join(path.parts[-2:]),
+                    "embedding": pred[i].tolist(),
+                    "predictions": predictions,
+                }
+            )
 
     spark = get_spark()
     df = spark.read.parquet(args["input"])
